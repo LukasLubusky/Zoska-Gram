179b0923dce57c9a3c24a2a15d66b1ee7895a609
AtlasPluto <alexrobertpetrovic@gmail.com>
2024-12-20 19:41:00 +0100

    Themes added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/NavBar";
 import AuthProvider from "../components/AuthProvider";
+import ThemeProvider from "@/components/ThemeProvider";
 
 export const metadata: Metadata = {
   title: "SnapZoška",
@@ -19,12 +20,14 @@ export default function RootLayout({
     <html lang="sk">
       <body>
         <AuthProvider>
+          <ThemeProvider>
           <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
             <main style={{ flexGrow: 1 }}>
               {children}
             </main>
           </div>
           <Navbar /> 
+          </ThemeProvider>
         </AuthProvider>
       </body>
     </html>
