ebb09b899f69f421e0dcd5a8a668c9980f6124b3
AtlasPluto <alexrobertpetrovic@gmail.com>
2025-03-03 09:04:32 +0100

    search fixed

--- src/app/(private)/hladanie/page.tsx
+++ src/app/(private)/hladanie/page.tsx
@@ -1,6 +1,5 @@
 // robertweb/src/app/hladanie/page.tsx
 
-import Typography from '@mui/material/Typography';
 import SearchView from '@/sections/SearchView';
 
 export const metadata = { title: 'Vyhladavanie | RobertWeb'};
